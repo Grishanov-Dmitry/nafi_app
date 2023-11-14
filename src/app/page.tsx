65a7c3117631da0bfd9bada70e02be4e87090d6c
dmitry-grishanov <grishanov.ds@gmail.com>
2023-11-14 23:25:53 +0100

    Add all components to Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
-import Image from "next/image";
+"use client";
+
+import { Header } from "./components/Header/Header";
+import { SearchMenu } from "./components/SearchMenu/SearchMenu";
+import { AddContactBlock } from "./components/AddContactBlock/AddContactBlock";
+import { ContactsTable } from "./components/ContactsTable/ContactsTable";
+import { useAppSelector } from "./redux/hooks";
+import { getIsLoged } from "./redux/slices/selectors";
+import { LoginPage } from "./components/LoginPage/LoginPage";
 
 export default function Home() {
-  return <div>Hello NAFI</div>;
+  const isLoged = useAppSelector(getIsLoged);
+
+  if (!isLoged) {
+    return <LoginPage />;
+  }
+
+  return (
+    <div className="container">
+      <Header />
+      <main>
+        <nav>
+          <AddContactBlock />
+          <SearchMenu />
+        </nav>
+        <ContactsTable />
+      </main>
+    </div>
+  );
 }
