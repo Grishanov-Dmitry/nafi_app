f323910a92406bfc0e2d8420b65bfb2870263010
dzmitry_hryshanau <grishanov.ds@gmail.com>
2023-12-12 21:55:34 +0100

    New deploy

--- next.config.js
+++ next.config.js
@@ -1,13 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'out',
-    typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        ignoreBuildErrors: true,
-      },
+  basePath: '/src',
+  output: 'export',
+
+  // Specify the base path for the exported pages
+  assetPrefix: '/src/',
+
+  // Specify the output directory for the export
+  // output: {
+  //   dir: 'out',
+  // },
+  typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
 }
 
 module.exports = nextConfig
--- out/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"42MKYsOIGPbJORmpMi/En5Bjq5p2xwNna/h6/4nMorM=\"\n}"
